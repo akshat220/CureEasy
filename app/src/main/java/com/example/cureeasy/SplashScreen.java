f3007133b796a643ac8879f0022159da23d14b9e
akshat220 <17ucs014@lnmiit.ac.in>
2019-10-13 00:28:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cureeasy/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.cureeasy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
