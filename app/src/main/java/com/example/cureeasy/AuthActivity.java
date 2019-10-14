34cae0974b6c82c1539cbc7bdecefe923c7d4701
akshat220 <17ucs014@lnmiit.ac.in>
2019-10-14 23:22:23 +0530

    Carousal modified

---
+++ app/src/main/java/com/example/cureeasy/AuthActivity.java
@@ -0,0 +1,26 @@
+package com.example.cureeasy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AuthActivity extends AppCompatActivity {
+
+    Button doctor, patient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+
+    public void doc(View v) {
+        startActivity(new Intent(this, DoctorHome.class));
+    }
+
+    public void pat(View v) {
+        startActivity(new Intent(this, PatientHome.class));
+    }
+}
